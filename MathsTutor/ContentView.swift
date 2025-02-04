d55523dfc2414d5dafea92d43a8934f02ae71d4e
Richard Gagg <r.gagg@outlook.com>
2025-02-05 10:33:06 +1100

    Initial Commit

---
+++ MathsTutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathsTutor
+//
+//  Created by Richard Gagg on 5/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathsTutor/MathsTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathsTutorApp.swift
+//  MathsTutor
+//
+//  Created by Richard Gagg on 5/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct MathsTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
